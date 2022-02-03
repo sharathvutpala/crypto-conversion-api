229ba78292a028c3e1f14d71930b4f7c73dcdc8e
sharath_vutpala <sharath.vutpala@axelerant.com>
2022-02-04 00:43:03 +0530

    added exception for invalid currency

--- api/currency.py
+++ api/currency.py
@@ -18,17 +18,23 @@ def home():
 
 @app.get("/currency/{selected_currency}")
 
-async def get_currency(selected_currency):
+async def get_currency(selected_currency: Optional[str] = "INR", max_length=3):
 
-    url = f'https://api.coinbase.com/v2/prices/spot?currency={selected_currency}'
-    response = requests.get(url)
-    return response.json()
+    try:
+        if selected_currency not in currency_list:
+            raise HTTPException(status_code=404, detail="Not currency")
+        else:
+            url = f'https://api.coinbase.com/v2/prices/spot?currency={selected_currency}'
+            response = requests.get(url)
+            data= response.json()
+            return data
+    except:
+        return "Can't connect to the API"
 
 @app.get("/health")
 async def get_health():
     API_ENDPOINT = "http://127.0.0.1:8000"
-    response = requests.get('http://127.0.0.1:8000')
-    #response = urllib.request.urlopen(API_ENDPOINT)
+    response = requests.get('http://64.225.84.48')
     if response.status_code == 200:
         return {"health":"ok"}
     else:
