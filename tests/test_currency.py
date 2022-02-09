bc4f2a5ad09923f08203c7ae598742d5d9c8172d
sharath_vutpala <sharath.vutpala@axelerant.com>
2022-02-09 20:00:09 +0530

    valid currency added to test_currency

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -7,7 +7,7 @@ from api.currency  import app
 client = TestClient(app)
 
 def test_curency():
-    response = client.get("/currency/ab")
+    response = client.get("/currency/EUR")
     assert response.json() != {"details":"Currency Details Not Found"}
     assert response.json() != {"details": "Currency length must be 3 Characters"} 
     assert response.status_code != 404
